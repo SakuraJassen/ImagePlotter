d2d12c5739bd5d5751950b0908912dcf0817edd7
SakuraJassen <tfd2@live.de>
2020-04-23 09:15:11 +0200

    made gifs reversable

--- reverser.py
+++ reverser.py
@@ -3,18 +3,21 @@ import progressbar
 
 fileName = "debug"
 fileEnding = ".gif"
-startOffset = 0
-endOffSet = 0
+startOffset = 1
+endOffSet = 10
+
+Metadata = {'fps': 30}
 
 with imageio.get_reader('C:\\Users\\Nutzer\\Documents\\Python\\out\\{0}{1}'.format(fileName, fileEnding)) as reader:
-    totalFrames = reader.get_length()
-    fps = totalFrames / reader.get_meta_data()["duration"] * 10
+    totalFrames = reader.get_length() # A Gif doesn't save the FPS so we either have to estimate it or get a User to provide it
+    #Metadata['fps'] = totalFrames / reader.get_meta_data()["duration"] * 20
     if "fps" in reader.get_meta_data():
         totalFrames = reader.count_frames() 
-        fps = reader.get_meta_data()["fps"]
+        Metadata['fps'] = reader.get_meta_data()["fps"]
+        Metadata['quality'] = 9
 
-    print("Creating File:'{0}{1}' with {2} FPS and total Frames of {3}".format(fileName, fileEnding, fps, totalFrames))
-    with imageio.get_writer('./out/{0}rev.mp4'.format(fileName), mode='I', fps=fps, quality=9) as writer:
+    print("Creating File:'{0}{1}' with {2} FPS and total Frames of {3}".format(fileName, fileEnding, Metadata['fps'], totalFrames))
+    with imageio.get_writer('./out/{0}rev{1}'.format(fileName, fileEnding), mode='I', **Metadata) as writer:
         images = []
         print("Reading...")
         for x in progressbar.progressbar(range(startOffset, totalFrames - endOffSet)):
