16a49f4e7bf883cd0c34628493ef48514dfc880d
SakuraJassen <tfd2@live.de>
2020-04-28 18:44:24 +0200

    Refactoring

--- draw.py
+++ draw.py
@@ -1,8 +1,14 @@
 import numpy
+from pygifsicle import optimize
 
 def drawRectangleFill(ndarray, p1x, p1y, p2x, p2y, r, g, b):
 	ndarray[p1y:p2y, p1x:p2x] = [r,g,b,255]
 
-def drawRectangle(ndarray, p1x, p1y, p2x, p2y, width, r, g, b):
+def drawRectangleWithBoarder(ndarray, p1x, p1y, p2x, p2y, bWidth, r, g, b):
 	drawRectangleFill(ndarray, p1x, p1y, p2x, p2y, 255, 255, 255)
-	drawRectangleFill(ndarray, p1x+width, p1y+width, p2x-width, p2y-width, r, g, b)
+	drawRectangleFill(ndarray, p1x + bWidth, p1y + bWidth, p2x - bWidth, p2y - bWidth, r, g, b)
+
+def optimizeGIF(path, out):
+    print("Optimizing GIF...")
+    if path[-4:] == '.gif':
+    	optimize(path, out) 
